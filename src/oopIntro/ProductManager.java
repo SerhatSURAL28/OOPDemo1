a0434209a9d1240a7467602be86179fd79822afd
serhat <serhat.sural@ericssonmsp.com>
2021-04-25 01:18:48 +0300

    first commit

---
+++ src/oopIntro/ProductManager.java
@@ -0,0 +1,10 @@
+package oopIntro;
+
+public class ProductManager {
+	
+	//sepete ekleme metodu similasyonu
+	public void addToCart(Product product) {
+		System.out.println("Sepete eklendi :"+product.name);
+	}
+	
+}
